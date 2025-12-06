097e3d6181ec7b09b45eaf5097a59f48e8506a64
dhruva207-ctrml <sdhruva207@gmail.com>
2025-12-06 20:38:22 +0530

    FIXED BUG ,animation
     typo mistake "info-bnt" converted to "info-btn,added hower and click effect

--- starter/script.js
+++ starter/script.js
@@ -1,3 +1,16 @@
-document.getElementById("info-bnt").addEventListener("click", () => {
+document.getElementById("info-btn").addEventListener("click", () => {
     alert("JS effect working!");
+});
+
+
+const name = document.getElementById("name");
+name.addEventListener("mouseenter", () => {
+    name.style.transform = "scale(1.2)";
+    name.style.transition = "transform 0.3s";
+});
+name.addEventListener("mouseleave", () => {
+    name.style.transform = "";
+});
+name.addEventListener("click", () => {
+    name.style.animation = "rainbow 2s infinite";
 });
